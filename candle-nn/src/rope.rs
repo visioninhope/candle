cff23e084c5f2979f222ce243d52145b591eaa7e
EricLBuehler <ericlbuehler@gmail.com>
2024-03-20 05:04:25 -0400

    Use mistral slow rope impl

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -35,31 +35,6 @@ impl RotaryEmbedding {
         dbg!(base);
         dbg!(head_dim);
         dbg!(max_position_embeddings);
-        {
-            let inv_freq: Vec<_> = (0..head_dim)
-                .step_by(2)
-                .map(|i| 1f32 / 10000f32.powf(i as f32 / head_dim as f32))
-                .collect();
-            let inv_freq_len = inv_freq.len();
-            let inv_freq = Tensor::from_vec(inv_freq, (1, inv_freq_len), device)?.to_dtype(DType::BF16)?;
-            let t = Tensor::arange(0u32, max_position_embeddings as u32, device)?
-                .to_dtype(DType::BF16)?
-                .reshape((max_position_embeddings, 1))?;
-            let freqs = t.matmul(&inv_freq)?;
-            let freqs = Tensor::cat(&[&freqs, &freqs], D::Minus1)?;
-            dbg!(freqs.mean_all());
-            dbg!(freqs.cos()?.mean_all());
-            return Ok(Self {
-                head_size: head_dim,
-                cos: freqs.cos()?,
-                sin: freqs.sin()?,
-                cache: freqs.contiguous()?.to_dtype(DType::BF16)?,
-                is_gpt_neox,
-            })
-        }
-        dbg!(base);
-        dbg!(head_dim);
-        dbg!(max_position_embeddings);
         let theta: Vec<_> = (0..head_dim)
             .step_by(2)
             .map(|i| 1f32 / base.powf(i as f32 / head_dim as f32))
@@ -237,7 +212,7 @@ impl RotaryEmbedding {
     }
 
     fn apply_rotary_emb(&self, x: &Tensor, seqlen_offsets: &[usize]) -> Result<Tensor> {
-        /*let (b_sz, n_head, seq_len, n_embd) = x.dims4()?;
+        let (b_sz, n_head, seq_len, n_embd) = x.dims4()?;
         let mut ropes = Vec::new();
         let x = x.reshape((b_sz, n_head, seq_len, n_embd / 2, 2))?;
         for (b, seqlen_offset) in zip(0..b_sz, seqlen_offsets) {
@@ -266,25 +241,6 @@ impl RotaryEmbedding {
             let rope = rope.flatten_from(D::Minus2)?;
             ropes.push(rope);
         }
-        Tensor::cat(&ropes, 0)*/
-        fn rotate_half(xs: &Tensor) -> Result<Tensor> {
-            let last_dim = xs.dim(D::Minus1)?;
-            let xs1 = xs.narrow(D::Minus1, 0, last_dim / 2)?;
-            let xs2 = xs.narrow(D::Minus1, last_dim / 2, last_dim - last_dim / 2)?;
-            Tensor::cat(&[&xs2.neg()?, &xs1], D::Minus1)
-        }
-
-        let (b_sz, _h, seq_len, _n_embd) = x.dims4()?;
-        let mut embeds = Vec::new();
-        for (b, seqlen_offset) in zip(0..b_sz, seqlen_offsets) {
-            let cos = self.cos.narrow(0, *seqlen_offset, seq_len)?;
-            let sin = self.sin.narrow(0, *seqlen_offset, seq_len)?;
-            let cos = cos.unsqueeze(0)?.unsqueeze(0)?; // (1, 1, seq_len, dim)
-            let sin = sin.unsqueeze(0)?.unsqueeze(0)?; // (1, 1, seq_len, dim)
-            let x_b = x.i(b)?.unsqueeze(0)?;
-            let x_embed = (x_b.broadcast_mul(&cos)? + rotate_half(&x_b)?.broadcast_mul(&sin))?;
-            embeds.push(x_embed);
-        }
-        Tensor::cat(&embeds, 0)
+        Tensor::cat(&ropes, 0)
     }
 }
