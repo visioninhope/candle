1e1d9868ef78ae24b051072ccebf3acd953159f9
EricLBuehler <ericlbuehler@gmail.com>
2024-03-22 04:42:10 -0400

    Debug

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -188,9 +188,10 @@ impl RotaryEmbedding {
         k: &mut Tensor,
     ) -> Result<()> {
         dbg!(q.mean_all());
-        *q = self.apply_rotary_emb(&*q, positions)?;
+        /**q = self.apply_rotary_emb(&*q, positions)?;
         *k = self.apply_rotary_emb(&*k, positions)?;
-        return Ok(());
+        return Ok(());*/
+        
         *q = q.contiguous()?;
         *k = k.contiguous()?;
         //let old_dtype = q.dtype();
@@ -199,12 +200,12 @@ impl RotaryEmbedding {
         //dbg!(q.mean_all());
         //dbg!(q.to_dtype(DType::BF16)?.mean_all());
         match (q.device(), k.device()) {
-            /*
+            
             #[cfg(feature = "cuda")]
             (Device::Cuda(dev), Device::Cuda(_)) => {
                 self.fused_rope(dev, positions_kernel, &*q, &*k)?;
             }
-            */
+            
 
             _ => {
                 *q = self.apply_rotary_emb(&*q, positions)?;
