d631ac0581eafbc259dcc5ce0b72260c5c7a05f2
EricLBuehler <ericlbuehler@gmail.com>
2024-03-22 11:11:42 -0400

    Debug

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -225,6 +225,8 @@ impl RotaryEmbedding {
         let x = x.reshape((b_sz, n_head_n_embd/self.head_size, seq_len, self.head_size))?;
         dbg!(x.shape());
         dbg!(&x.i(0).unwrap().to_vec3::<half::bf16>()?[0][0][0..10]);
+        dbg!(&x.i(0).unwrap().to_vec3::<half::bf16>()?[2][0][0..10]);
+        dbg!(&x.i(0).unwrap().to_vec3::<half::bf16>()?[2][2][0..10]);
         let (b_sz, n_head, seq_len, n_embd) = x.dims4()?;
         let mut ropes = Vec::new();
         let x = x.reshape((b_sz, n_head, seq_len, n_embd / 2, 2))?;
