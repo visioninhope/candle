62b76c791908b3ebf186b2dc225dc03cbdc55a0e
EricLBuehler <ericlbuehler@gmail.com>
2024-03-22 10:58:53 -0400

    Debug

--- candle-nn/src/rope.rs
+++ candle-nn/src/rope.rs
@@ -223,6 +223,7 @@ impl RotaryEmbedding {
     fn apply_rotary_emb(&self, x: &Tensor, seqlen_offsets: &[usize]) -> Result<Tensor> {
         let (b_sz, seq_len, n_head_n_embd) = x.dims3()?;
         let x = x.reshape((b_sz, n_head_n_embd/self.head_size, seq_len, self.head_size))?;
+        dbg!(x.shape());
         let (b_sz, n_head, seq_len, n_embd) = x.dims4()?;
         let mut ropes = Vec::new();
         let x = x.reshape((b_sz, n_head, seq_len, n_embd / 2, 2))?;
