fefdf3764e4f725494bc6b14ada1af69360258c9
EricLBuehler <ericlbuehler@gmail.com>
2024-03-24 16:43:31 -0400

    Reshape input for rank 2

--- candle-nn/src/layer_norm.rs
+++ candle-nn/src/layer_norm.rs
@@ -276,7 +276,7 @@ impl crate::Module for RmsNorm {
             let (bs, s, h) = xs.dims3()?;
             let xs = xs.reshape((bs*s, h))?;
             let res = candle_layer_norm::rms_norm(xs, self.0.weight(), None, self.0.eps as f32)?;
-            res.reshape((bs, s, h))?
+            res.reshape((bs, s, h))
         }
         #[cfg(not(feature = "cuda"))]
         { self.0.forward(xs) }
